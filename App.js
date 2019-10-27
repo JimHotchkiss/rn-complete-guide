9a402019bbdfb25105f1d0b34c3c7eedb3fdfb51
github username <github email address>
2019-10-26 20:04:50 -0700

    Learned about adding text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
 // [initalState, changedState]
@@ -12,7 +12,7 @@ export default function App() {
 
   // Display user input 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
   }
   
   
@@ -30,25 +30,49 @@ export default function App() {
             onPress={addGoalHandler}
             />
       </View>
-      <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
-      </View>
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem = {itemData => 
+          <View
+          style={styles.outPutView}>
+            <Text style={styles.outPutText} 
+              >{itemData.item.value}
+            </Text>
+        </View>
+        }/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    paddingTop: 40,
+    padding: 30
   }, 
   inputContainer: {
-    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+
   }, 
   inputText: {
-    width: '85%', 
+    width: '80%', 
     borderColor: 'black',
-    marginRight: 10, 
+    marginRight: 20, 
     borderWidth: 1, 
     padding: 10
+  },
+  outPutView: {
+    width: '100%',
+    height: 'auto',
+    backgroundColor: '#f4f4f4',
+    marginVertical: 5,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    padding: 10
+  },
+  outPutText: {
+    fontSize: 16,
   }
 });
